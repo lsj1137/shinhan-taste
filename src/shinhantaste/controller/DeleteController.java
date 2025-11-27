fa75fcb3888341e3ee18a251c2af07f37c659520
limsejun <lsj1137@naver.com>
2025-11-27 11:07:58 +0900

    feat, refactor: 글 삭제 기능 글 상세 메뉴에 추가, SelectController switch-case문 세개 중첩 분리, 글 상세 메뉴 출력 추가

--- src/shinhantaste/controller/DeleteController.java
+++ src/shinhantaste/controller/DeleteController.java
@@ -12,14 +12,8 @@ public class DeleteController {
 	Scanner sc = new Scanner(System.in);
 	ArticleService articleService = new ArticleService();
 
-	public void execute(List<ArticleDTO> articleList) {
-		ArticleView.printDeleteMenu();
-		int deleteId = sc.nextInt();
-		for (ArticleDTO articleDTO : articleList) {
-			if (articleDTO.getArticleId().equals(deleteId)) {
-				String result = articleService.deleteArticle(deleteId);
-				System.out.println(result);
-			}
-		}
+	public String execute(ArticleDTO articleDTO) {
+		String result = articleService.deleteArticle(articleDTO.getArticleId());
+		return result;
 	}
 }
--- src/shinhantaste/view/ArticleView.java
+++ src/shinhantaste/view/ArticleView.java
@@ -41,9 +41,8 @@ public class ArticleView {
             case 4 -> "양식"; case 7 -> "기타"; default -> "기타";
         };
     }
-}
 	
-	public static void printDeleteMenu() {
-		System.out.println("삭제할 글을 선택해주세요 >> ");
+	public static void printDetailMenu() {
+		System.out.print("1. 수정  2. 삭제  0. 글 목록으로\n선택>> ");
 	}
 }
